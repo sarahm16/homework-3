24be6cb6e04fdbd6b2620a4d81be68da52f4be37
Sarah Carter <sarahmarie.carter@yahoo.com>
2019-12-17 10:41:35 -0800

    created prompts that store user input as parameters for password

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-specialCharacters = []
+
+// assigning variables to html elements from index.html
+let passwordEl = document.querySelector("#password");
+let buttonEl = document.querySelector('#generate');
+let copyEl = document.querySelector('#copy');
+
+
+// prompting the user to choose parameters for password
+let length = prompt('Choose a password length between 8 and 128 characters');
+length = parseFloat(length);
+if(length < '8' || length > '128') {
+    alert('please choose a length between 8 and 128 characters');
+}
+
+let special = confirm('Would you like special characters in the password?');
+let lowercase = confirm('Would you like lowercase letters in the password?');
+let uppercase = confirm('Would you like uppercase characters in the password?');
+let numeric = confirm('Would you like numeric characters in the password?');
+
+
+
+
+
+
+
+
+
+
+
+
+// adding event listeners to generate password and copy to clipboard buttons
+buttonEl.addEventListener('click', function() {
+    passwordEl.textContent = password;
+});
+
+copyEl.addEventListener('click', function() {
+
+});

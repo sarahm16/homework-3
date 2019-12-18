bc871da1f3b0c080a94f7cd355a77510da64e774
Sarah Carter <sarahmarie.carter@yahoo.com>
2019-12-18 11:26:28 -0800

    changed password color to teal

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ let copyEl = document.querySelector('#copy');
 
 
 // prompting the user to choose parameters for password
-// let length = prompt('Choose a password length between 8 and 128 characters');
-// length = parseFloat(length);
-// if(length < '8' || length > '128') {
-//     alert('please choose a length between 8 and 128 characters');
-//     length = prompt('Choose a password length between 8 and 128 characters');
-// }
+let length = prompt('Choose a password length between 8 and 128 characters');
+length = parseFloat(length);
+if(length < '8' || length > '128') {
+    alert('please choose a length between 8 and 128 characters');
+    length = prompt('Choose a password length between 8 and 128 characters');
+}
 
-// let special = confirm('Would you like special characters in the password?');
-// let lowercase = confirm('Would you like lowercase letters in the password?');
-// let uppercase = confirm('Would you like uppercase characters in the password?');
-// let numeric = confirm('Would you like numeric characters in the password?');
+let special = confirm('Would you like special characters in the password?');
+let lowercase = confirm('Would you like lowercase letters in the password?');
+let uppercase = confirm('Would you like uppercase characters in the password?');
+let numeric = confirm('Would you like numeric characters in the password?');
 
 // creating an array of booleans
 let charType = [special, lowercase, uppercase, numeric];
@@ -56,6 +56,7 @@ generatePassword(length);
 // adding event listeners to generate password and copy to clipboard buttons
 buttonEl.addEventListener('click', function() {
     passwordEl.textContent = password;
+    passwordEl.style.color = 'teal';
 });
 
 copyEl.addEventListener('click', function() {
